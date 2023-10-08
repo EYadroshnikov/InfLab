38b1291b3bb403c33a937e886b790c48fc9e3730
EYadroshnikov <yadegor1375@gmail.com>
2023-10-08 14:26:21 +0300

    added additional functionality to lab02/18.py

--- lab02/18.py
+++ lab02/18.py
@@ -1,10 +1,12 @@
 while True:
-        try:
-            prev = int(input("Введите целое число: "))
-            break
-        except ValueError:
-            print("Ошибка! Введите целое число.")
+    try:
+        prev = int(input("Введите целое число: "))
+        break
+    except ValueError:
+        print("Ошибка! Введите целое число.")
+
 min_abs_dif = float('inf')
+max_abs_dif = 0
 
 for i in range(9):
     while True:
@@ -17,6 +19,12 @@ for i in range(9):
     cur_abs_dif = abs(cur - prev)
     if cur_abs_dif < min_abs_dif:
         min_abs_dif = cur_abs_dif
+    if cur_abs_dif > max_abs_dif:
+        max_abs_dif = cur_abs_dif
     prev = cur
 
-print(min_abs_dif)
+print(f'min: {min_abs_dif}')
+print(f'max: {max_abs_dif}')
+
+# Сделать повторный ввод при ошибке
+# Вывести максимальную разницу между разностью всех соседних чисел
