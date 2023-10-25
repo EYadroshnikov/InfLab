b2a18da6a09ce3984dc481135bebcd45f4423e80
EYadroshnikov <yadegor1375@gmail.com>
2023-10-25 15:56:51 +0300

    added descriprion of lab03 and tests to lab04

--- lab04/main.py
+++ lab04/main.py
@@ -15,9 +15,9 @@ def main():
     arr1 = [432, 786, 230, 574, 954, 719, 381, 567, 853]
     arr2 = [125, 789, 451, 893, 322, 667, 423, 540, 998, 234]
     arr3 = [764, 312, 678, 287, 451, 832, 56, 231, 789]
-    print(f'{arr1}: {delete_local_maxes(arr1)}')
-    print(f'{arr2}: {delete_local_maxes(arr2)}')
-    print(f'{arr3}: {delete_local_maxes(arr3)}')
+    print(f'test1:\n    original: {arr1}\n    sorted: {sort.quicksort(arr1)}\n    without local maxes: {delete_local_maxes(arr1)}\n')
+    print(f'test2:\n    original: {arr2}\n    sorted: {sort.quicksort(arr2)}\n    without local maxes: {delete_local_maxes(arr2)}\n')
+    print(f'test3:\n    original: {arr3}\n    sorted: {sort.quicksort(arr3)}\n    without local maxes: {delete_local_maxes(arr3)}\n')
 
 
 if __name__ == '__main__':
